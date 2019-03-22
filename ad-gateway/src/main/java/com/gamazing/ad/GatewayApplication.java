67edc8578a05f925974c5092ced112d540477a4a
gaojie <873037900@qq.com>
2019-03-22 17:59:03 +0800

    x

---
+++ ad-gateway/src/main/java/com/gamazing/ad/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gamazing.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ ad-service/ad-common/src/main/java/com/gamazing/ad/vo/CommonResponse.java
@@ -0,0 +1,23 @@
+package com.gamazing.ad.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommonResponse<T> implements Serializable {
+
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
